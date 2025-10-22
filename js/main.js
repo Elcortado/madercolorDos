b8e004a6e0435ea964e1fef563e02267deae990b
Elcortado <josealejandrobermudez@hotmail.com>
2025-10-22 01:02:29 -0300

    whts

--- js/main.js
+++ js/main.js
@@ -13,10 +13,7 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+   
     
     
     // Sticky Navbar
